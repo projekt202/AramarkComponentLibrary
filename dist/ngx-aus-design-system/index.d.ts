30fe9cb88f1c17b4d2eec69679fa09a474caaac1
Andres Liu <andres.liu@projekt202.com>
2023-03-01 08:59:34 -0500

    Untrack /dist folder and publish library via GitHub packages

--- dist/ngx-aus-design-system/index.d.ts
+++ dist/ngx-aus-design-system/index.d.ts
@@ -1,5 +1,5 @@
 /**
  * Generated bundle index. Do not edit.
  */
-/// <amd-module name="ngx-aus-design-system" />
+/// <amd-module name="@projekt202/ngx-aus-design-system" />
 export * from './public-api';
