bdbb490afe889199784cf3a8ac9a9a1561b250de
Andres Liu <andres.liu@projekt202.com>
2023-03-16 16:20:15 -0500

    ADS-55 add support for new naming conventions

--- projects/ngx-aus-design-system/src/lib/shared/components/button/button.module.ts
+++ projects/ngx-aus-design-system/src/lib/shared/components/button/button.module.ts
@@ -1,3 +1,6 @@
+import { Array2StringPipeModule } from "./../../pipes/array2string/array2string.module";
+import { SanitizePipeModule } from "./../../pipes/sanitize/sanitize.module";
+import { TypographyModule } from "./../typography/typography.module";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
@@ -7,7 +10,15 @@ import { ButtonComponent } from "./button.component";
 
 @NgModule({
   declarations: [ButtonComponent],
-  imports: [CommonModule, NgxSmoothScrollModule, RouterModule, IconModule],
+  imports: [
+    CommonModule,
+    NgxSmoothScrollModule,
+    RouterModule,
+    IconModule,
+    TypographyModule,
+    SanitizePipeModule,
+    Array2StringPipeModule,
+  ],
   providers: [],
   exports: [ButtonComponent],
 })
--- projects/ngx-aus-design-system/src/lib/shared/components/button/button.stories.ts
+++ projects/ngx-aus-design-system/src/lib/shared/components/button/button.stories.ts
@@ -1,16 +1,25 @@
-import { RouterTestingModule } from '@angular/router/testing';
-import { NgxSmoothScrollModule } from '@boatzako/ngx-smooth-scroll';
-import { Story, Meta, moduleMetadata } from '@storybook/angular';
-import { IconComponent } from '../icon/icon.component';
-import { ButtonComponent } from './button.component';
+import { Array2StringPipeModule } from "./../../pipes/array2string/array2string.module";
+import { SanitizePipeModule } from "./../../pipes/sanitize/sanitize.module";
+import { TypographyModule } from "./../typography/typography.module";
+import { RouterTestingModule } from "@angular/router/testing";
+import { NgxSmoothScrollModule } from "@boatzako/ngx-smooth-scroll";
+import { Story, Meta, moduleMetadata } from "@storybook/angular";
+import { IconComponent } from "../icon/icon.component";
+import { ButtonComponent } from "./button.component";
 
 export default {
-  title: 'Button',
+  title: "Button",
   component: ButtonComponent,
   decorators: [
     moduleMetadata({
       declarations: [ButtonComponent, IconComponent],
-      imports: [RouterTestingModule, NgxSmoothScrollModule],
+      imports: [
+        RouterTestingModule,
+        NgxSmoothScrollModule,
+        TypographyModule,
+        SanitizePipeModule,
+        Array2StringPipeModule,
+      ],
     }),
   ],
 } as Meta;
@@ -21,144 +30,154 @@ const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
 
 export const White = Template.bind({});
 White.args = {
-  text: 'Get in touch with a team member',
-  variant: 'white',
-  route: '/test',
-  target: '_self',
+  text: "Get in touch with a team member",
+  variant: "white",
+  route: "/test",
+  target: "_self",
 };
 
 export const Black = Template.bind({});
 Black.args = {
-  text: 'Get in touch with a team member',
-  variant: 'black',
-  route: '/test',
-  target: '_self',
+  text: "Get in touch with a team member",
+  variant: "black",
+  route: "/test",
+  target: "_self",
 };
 
 export const Red = Template.bind({});
 Red.args = {
-  text: 'Get in touch with a team member',
-  variant: 'red',
-  route: '/test',
-  target: '_self',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  text: 'Get in touch with a team member',
-  variant: 'red',
-  route: '/test',
-  target: '_self',
-  size: 'small',
-};
-
-export const Medium = Template.bind({});
-Medium.args = {
-  text: 'Get in touch with a team member',
-  variant: 'red',
-  route: '/test',
-  target: '_self',
-  size: 'medium',
-  icon: {
-    name: 'package',
-    position: 'right',
-  },
+  text: "Get in touch with a team member",
+  variant: "red",
+  route: "/test",
+  target: "_self",
 };
 
 export const ExternalLink = Template.bind({});
 ExternalLink.args = {
-  text: 'Get in touch with a team member',
-  variant: 'red',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "red",
+  href: "https://www.google.com",
+  target: "_blank",
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   disabled: true,
-  text: 'Get in touch with a team member',
-  variant: 'black',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "black",
+  href: "https://www.google.com",
+  target: "_blank",
 };
 
 export const Icon = Template.bind({});
 Icon.args = {
-  text: 'Get in touch with a team member',
-  variant: 'black',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "black",
+  href: "https://www.google.com",
+  target: "_blank",
   icon: {
-    name: 'quote',
-    position: 'left',
+    name: "quote",
+    position: "left",
   },
 };
 
 export const IconDisabled = Template.bind({});
 IconDisabled.args = {
-  text: 'Get in touch with a team member',
-  variant: 'black',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "black",
+  href: "https://www.google.com",
+  target: "_blank",
   icon: {
-    name: 'pickup',
-    position: 'left'
+    name: "pickup",
+    position: "left",
   },
   disabled: true,
 };
 
 export const Transparent = Template.bind({});
 Transparent.args = {
-  text: 'Get in touch with a team member',
-  variant: 'transparent',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "transparent",
+  href: "https://www.google.com",
+  target: "_blank",
   icon: {
-    name: 'pickup',
-    position: 'left'
+    name: "pickup",
+    position: "left",
   },
 };
 
 export const TransparentDisabled = Template.bind({});
 TransparentDisabled.args = {
-  text: 'Get in touch with a team member',
-  variant: 'transparent',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "transparent",
+  href: "https://www.google.com",
+  target: "_blank",
   icon: {
-    name: 'pickup',
-    position: 'left'
+    name: "pickup",
+    position: "left",
   },
   disabled: true,
 };
 
 export const Outline = Template.bind({});
 Outline.args = {
-  text: 'Get in touch with a team member',
-  variant: 'outline',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "outline",
+  href: "https://www.google.com",
+  target: "_blank",
   disabled: false,
 };
 
 export const OutlineWithIcon = Template.bind({});
 OutlineWithIcon.args = {
-  text: 'Get in touch with a team member',
-  variant: 'outline',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "outline",
+  href: "https://www.google.com",
+  target: "_blank",
   icon: {
-    name: 'pickup',
-    position: 'left'
+    name: "pickup",
+    position: "left",
   },
   disabled: false,
 };
 
 export const OutlineDisabled = Template.bind({});
 OutlineDisabled.args = {
-  text: 'Get in touch with a team member',
-  variant: 'outline',
-  href: 'https://www.google.com',
-  target: '_blank',
+  text: "Get in touch with a team member",
+  variant: "outline",
+  href: "https://www.google.com",
+  target: "_blank",
   disabled: true,
+};
+
+export const PrimaryBrand = Template.bind({});
+PrimaryBrand.args = {
+  text: "Get in touch with a team member",
+  variant: "primary-brand",
+  route: "/test",
+  size: "large",
+};
+
+export const PrimaryNeutral = Template.bind({});
+PrimaryNeutral.args = {
+  text: "Get in touch with a team member",
+  variant: "primary-neutral",
+  route: "/test",
+  size: "large",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  text: "Get in touch with a team member",
+  variant: "secondary",
+  route: "/test",
+  size: "medium",
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  text: "Get in touch with a team member",
+  variant: "tertiary",
+  route: "/test",
+  size: "small",
 };
