4b33a2f11b31ebcbebfdbd31b41bbbf9d866c1ca
Andres Liu <andres.liu@projekt202.com>
2023-03-16 14:25:48 -0500

    ADS-6 display colors in rgb

--- src/app/documentation/pages/colors/colors.component.ts
+++ src/app/documentation/pages/colors/colors.component.ts
@@ -240,4 +240,14 @@ export class ColorsComponent {
       ],
     },
   ];
+
+  toRgb(hex: string) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(
+          result[3],
+          16
+        )})`
+      : "";
+  }
 }
