66d334268238cb047d20024278f28d51a29b65a1
Andres Liu <andres.liu@projekt202.com>
2023-03-17 16:51:04 -0500

    ADS-28 add preset stories and adjust titles

--- projects/ngx-aus-design-system/src/lib/shared/components/typography/typography.stories.ts
+++ projects/ngx-aus-design-system/src/lib/shared/components/typography/typography.stories.ts
@@ -160,8 +160,62 @@ LegacyLinkDisabled.args = {
   disabled: true,
 };
 
-export const StandaloneLinkWithTemplateIcon = Template.bind({});
-StandaloneLinkWithTemplateIcon.args = {
+export const Preset_1 = Template.bind({});
+Preset_1.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-1"],
+};
+
+export const Preset_2 = Template.bind({});
+Preset_2.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-2"],
+};
+
+export const Preset_3 = Template.bind({});
+Preset_3.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-3"],
+};
+
+export const Preset_4 = Template.bind({});
+Preset_4.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-4"],
+};
+
+export const Preset_5 = Template.bind({});
+Preset_5.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-5"],
+};
+
+export const Preset_6 = Template.bind({});
+Preset_6.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-6"],
+};
+
+export const Preset_7 = Template.bind({});
+Preset_7.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-7"],
+};
+
+export const Preset_8 = Template.bind({});
+Preset_8.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-8"],
+};
+
+export const Preset_9 = Template.bind({});
+Preset_9.args = {
+  text: "Hello world!",
+  presetClassNames: ["text-preset-9"],
+};
+
+export const LinkStandaloneWithIconTemplate = Template.bind({});
+LinkStandaloneWithIconTemplate.args = {
   variant: "link",
   text: "Standalone Link",
   route: "/test",
@@ -175,8 +229,8 @@ StandaloneLinkWithTemplateIcon.args = {
   disabled: false,
 };
 
-export const StandaloneLinkWithIconName = Template.bind({});
-StandaloneLinkWithIconName.args = {
+export const LinkStandaloneWithIconName = Template.bind({});
+LinkStandaloneWithIconName.args = {
   variant: "link",
   text: "Standalone Link",
   route: "/test",
@@ -189,9 +243,19 @@ StandaloneLinkWithIconName.args = {
   disabled: false,
 };
 
-export const InlineLink = Template.bind({});
-InlineLink.args = {
-  variant: "p",
+export const LinkStandaloneDisabled = Template.bind({});
+LinkStandaloneDisabled.args = {
+  variant: "link",
+  text: "Standalone Link",
+  route: "/test",
+  linkStandalone: true,
+  linkSize: "large",
+  disabled: true,
+};
+
+export const LinkInline = Template.bind({});
+LinkInline.args = {
+  presetClassNames: ["text-preset-7"],
   text: "This paragraph contains an <a href='#' class='link link--size-medium'><span class='link__label left'>Inline Link</span><i class='link__icon right'><svg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M16 16H2V2H9V0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.1 18 18 17.1 18 16V9H16V16ZM11 0V2H14.59L4.76 11.83L6.17 13.24L16 3.41V7H18V0H11Z' fill='#EB002A'/></svg></i></a> with an icon and another without: <a href='/test' class='link'><span class='link__label'>Another Link</span></a>",
   route: "/test",
 };
